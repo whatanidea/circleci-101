3aaaa248806bea8cc85104f5a567b8cfc3e9f391
Abbas <muntazir.abbas@accenture.com>
2019-06-24 13:00:28 +0200

    new files add

--- server/boot/authentication.js
+++
@@ -1,9 +0,0 @@
-// Copyright IBM Corp. 2014,2019. All Rights Reserved.
-// Node module: loopback-getting-started
-
-'use strict';
-
-module.exports = function enableAuthentication(server) {
-  // enable authentication
-  server.enableAuth();
-};
